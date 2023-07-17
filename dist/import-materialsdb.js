af3c26d172d731053bb1854e9e290e8251a41367
MartinoTschudi <m.tschudi@swiss-property.ch>
2023-07-17 18:15:20 +0200

    updated sp import
    working version for the upload of the catalog on the 17.07.2023

--- dist/import-materialsdb.js
+++ dist/import-materialsdb.js
@@ -13,10 +13,17 @@ export async function importMaterialsDb(url, auth, category, verbose) {
     const authToken = await fs.readFile(auth, { encoding: 'utf8', flag: 'r' });
     let kbobLookup = {};
     const kbobLookupCsv = await readCsv('data/materialsdb_kbob2016_lookuptable.csv');
+    const groupTranslation = await readCsv('data/material_group_translation.csv');
+    let uniquenames = true;
+    let usednames = [];
+
+    
     for (const row of kbobLookupCsv) {
         if (row.KBOB_ID)
             kbobLookup[row.GUID] = row.KBOB_ID;
     }
+
+
     const producerIndexResponse = await request.get('http://www.materialsdb.org/download/ProducerIndex.xml')
         .set('Accept', 'application/xhtml+xml,application/xml;')
         .responseType('blob');
@@ -52,6 +59,8 @@ export async function importMaterialsDb(url, auth, category, verbose) {
         .set('Accept-Encoding', 'gzip, deflate, br')
         .set('Accept-Language', 'en-US,en;q=0.5')
         .set('Content-Type', 'application/json');
+
+
     let lcaAttributeGroupId = null;
     for (const g of attributeGroups.body) {
         if (g.name == lcaAttributeGroup) {
@@ -88,89 +97,86 @@ export async function importMaterialsDb(url, auth, category, verbose) {
             .set('Accept-Encoding', 'gzip, deflate, br')
             .set('Accept-Language', 'en-US,en;q=0.5')
             .set('Content-Type', 'application/json');
+            
         const producerCategoryId = producerCategory.body.id;
         for (const material of producerData.materials.material) {
+
+            const name = getTranslatedString(material.information[0].names[0], "name", "de");
+            const productName = `${name} (${company.$.name})`;
+
+            if (uniquenames) {
+                if (usednames.includes(productName)) {
+                    console.log(`\tSkipped '${productName}' - not unique`);
+                    continue;
+                } else {
+                    usednames.push(productName);
+                }
+            }
+
+
+            const owner = "Swiss Property AG";
+            const license = "Creative Commons Attribution-ShareAlike 4.0 International License";
+            const produ = company.$.name;
+
+            let group;
+
+            if (material.information[0].$) {
+                group = material.information[0].$.group;
+
+                for (const row of groupTranslation) {
+                    if (row.mat_en == group)
+                        group = row.mat_de;
+                }
+            }
+
+
+            const sourceEPE = material.$.id+ " - materialsdb.org";
+
             if (material.$.type != "simple") {
                 if (verbose)
                     console.log(`Skipping entry that is not of type 'simple'`);
                 continue;
             }
-            const name = getTranslatedString(material.information[0].names[0], "name", "de");
+            
             const explanation = material.information[0].explanations ? getTranslatedString(material.information[0].explanations[0], "explanation", "de") : null;
             console.log(`\tProcessing entry '${name}'`);
-            let numLayers = material.layers[0].layer.length;
-            let idx = 0;
+
+            
             for (const layer of material.layers[0].layer) {
-                let thick, length, width, density, therm_capa, lambda;
+                
+                let thick, density, therm_capa, lambda, fireclass, fireresis, firecomb, firesmoke, firereact;
                 if (layer.geometry && layer.geometry[0].$) {
                     thick = layer.geometry[0].$.thick;
-                    length = layer.geometry[0].$.length;
-                    width = layer.geometry[0].$.width;
+                    // length = layer.geometry[0].$.length;
+                    // width = layer.geometry[0].$.width;
                 }
                 if (layer.physical && layer.physical[0].$) {
                     density = layer.physical[0].$.density;
+                }
+                if (layer.security && layer.security[0].$) {
+                    fireclass = layer.security[0].$.FireClass;
+                    fireresis = layer.security[0].$.FireResis;
+                    firecomb = layer.security[0].$.FireComb;
+                    firesmoke = layer.security[0].$.FireSmoke;
+                    firereact = layer.security[0].$.FireReact;
                 }
                 if (layer.thermal && layer.thermal[0].$) {
                     therm_capa = layer.thermal[0].$.therm_capa;
                     lambda = layer.thermal[0].$.lambda_value;
                 }
-                let nameSuffix = "";
-                if (!singleLayer && numLayers > 1) {
-                    if (thick && length && width)
-                        nameSuffix = ` ${thick}/${length}/${width}`;
-                    else
-                        nameSuffix = ` ${idx + 1}`;
-                }
-                let lcaCode = null;
-                if (layer.lca) {
-                    for (const lcaEntry of layer.lca) {
-                        if (lcaEntry && lcaEntry.$ && lcaEntry.$.database == lcaDatabase && lcaEntry.lcaversion && lcaEntry.lcaversion.length) {
-                            for (const lcaVersion of lcaEntry.lcaversion) {
-                                if (lcaVersion && lcaVersion.$ && lcaVersionNames.includes(lcaVersion.$.id)) {
-                                    lcaCode = lcaVersion.$.code;
-                                    if (lcaVersionNames[0] == lcaVersion.$.id)
-                                        break;
-                                }
-                            }
-                        }
+
+
+                let options = [];
+                for (const l of material.layers[0].layer) {
+                    if (l.geometry && l.geometry[0].$) {
+                        // console.log("LAYER "+layer.geometry[0].$.thick)
+                        // console.log("L "+l.geometry[0].$.thick)
+                        options.push(l.geometry[0].$.thick);
                     }
+                    
                 }
-                if (!lcaCode) {
-                    console.log(`\tSkipping material due to missing LCA code for database '${lcaDatabase}'`);
-                    continue;
-                }
-                if (!kbobLookup[lcaCode]) {
-                    console.log(`\tSkipping material because LCA key '${lcaCode}' could not be found in lookup table'`);
-                    continue;
-                }
-                let lcaProductId = null;
-                if (!lcaProductsCache[lcaCode]) {
-                    const prefixes = ["KBOB2022", "KBOB2016"];
-                    for (const prefix of prefixes) {
-                        const lcaProductName = `${prefix}-${kbobLookup[lcaCode]}`;
-                        const lcaProducts = await request.get(new URL(`/catalogues/${selectedCatalogueId}/products?type=REFERENCE_MATERIAL&productKey=${lcaProductName}&limit=10`, url).href)
-                            .set('Authorization', authToken)
-                            .set('Content-Type', 'application/json')
-                            .set('Accept', 'application/, json')
-                            .set('Accept-Encoding', 'gzip, deflate, br')
-                            .set('Accept-Language', 'en-US,en;q=0.5')
-                            .set('Content-Type', 'application/json');
-                        if (!lcaProducts || !lcaProducts.body || !lcaProducts.body.length || !lcaProducts.body[0] || !lcaProducts.body[0].id) {
-                            continue;
-                        }
-                        lcaProductId = lcaProducts.body[0].id;
-                        break;
-                    }
-                    if (!lcaProductId) {
-                        console.log(`\tSkipping material because linked LCA product with key '${lcaCode}' could not be found'`);
-                        continue;
-                    }
-                    lcaProductsCache[lcaCode] = lcaProductId;
-                }
-                else {
-                    lcaProductId = lcaProductsCache[lcaCode];
-                }
-                const productName = `${name}${nameSuffix} (${company.$.name})`;
+
+                
                 const newProduct = await request.post(new URL('/products', url).href)
                     .send({
                     "name": productName,
@@ -185,6 +191,79 @@ export async function importMaterialsDb(url, auth, category, verbose) {
                     .set('Accept-Language', 'en-US,en;q=0.5')
                     .set('Content-Type', 'application/json');
                 const id = newProduct.body.id;
+
+
+                breakme: {
+                    let lcaCode = null;
+                    if (layer.lca) {
+                        for (const lcaEntry of layer.lca) {
+                            if (lcaEntry && lcaEntry.$ && lcaEntry.$.database == lcaDatabase && lcaEntry.lcaversion && lcaEntry.lcaversion.length) {
+                                for (const lcaVersion of lcaEntry.lcaversion) {
+                                    if (lcaVersion && lcaVersion.$ && lcaVersionNames.includes(lcaVersion.$.id)) {
+                                        lcaCode = lcaVersion.$.code;
+                                        if (lcaVersionNames[0] == lcaVersion.$.id)
+                                            break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if (!lcaCode) {
+                        console.log(`\tno LCA data for  '${name}'`);
+                        //console.log(`\tSkipping material due to missing LCA code for database '${lcaDatabase}'`);
+                        break breakme;
+                    }
+                    if (!kbobLookup[lcaCode]) {
+                        //console.log(`\tSkipping material because LCA key '${lcaCode}' could not be found in lookup table'`);
+                        console.log(`\tno LCA data for  '${name}'`);
+                        break breakme;
+                    }
+                    let lcaProductId = null;
+                    if (!lcaProductsCache[lcaCode]) {
+                        const prefixes = ["KBOB2022", "KBOB2016"];
+                        for (const prefix of prefixes) {
+                            const lcaProductName = `${prefix}-${kbobLookup[lcaCode]}`;
+                            const lcaProducts = await request.get(new URL(`/catalogues/${selectedCatalogueId}/products?type=REFERENCE_MATERIAL&productKey=${lcaProductName}&limit=10`, url).href)
+                                .set('Authorization', authToken)
+                                .set('Content-Type', 'application/json')
+                                .set('Accept', 'application/, json')
+                                .set('Accept-Encoding', 'gzip, deflate, br')
+                                .set('Accept-Language', 'en-US,en;q=0.5')
+                                .set('Content-Type', 'application/json');
+                            if (!lcaProducts || !lcaProducts.body || !lcaProducts.body.length || !lcaProducts.body[0] || !lcaProducts.body[0].id) {
+                                console.log(`no LCA data for  '${name}'`);
+                                break breakme;
+                            }
+                            lcaProductId = lcaProducts.body[0].id;
+                            break;
+                        }
+                        if (!lcaProductId) {
+                            //console.log(`\tSkipping material because linked LCA product with key '${lcaCode}' could not be found'`);
+                            console.log(`\tno LCA data for  '${name}'`);
+                            break breakme;
+                        }
+                        lcaProductsCache[lcaCode] = lcaProductId;
+                    }
+                    else {
+                        lcaProductId = lcaProductsCache[lcaCode];
+                    }
+
+                    const materialListLink = await request.post(new URL(`/reference-material-links`, url).href)
+                    .send({
+                    "materialId": id,
+                    "referenceMaterialId": lcaProductId,
+                    "attributeGroupId": lcaAttributeGroupId
+                    })
+                
+                    .set('Authorization', authToken)
+                    .set('Content-Type', 'application/json')
+                    .set('Accept', 'application/, json')
+                    .set('Accept-Encoding', 'gzip, deflate, br')
+                    .set('Accept-Language', 'en-US,en;q=0.5')
+                    .set('Content-Type', 'application/json');
+                }
+
+
                 const attributeIds = {};
                 for (const attr of newProduct.body.attributes) {
                     attributeIds[attr.name] = attr.id;
@@ -200,23 +279,49 @@ export async function importMaterialsDb(url, auth, category, verbose) {
                         value: value
                     });
                 }
-                if (thick)
-                    pushAttr("vyzn.catalogue.Thickness", parseFloat(thick));
-                if (width)
-                    pushAttr("vyzn.catalogue.Width", parseFloat(width));
+
+                
+                if (options)
+                    if (options != []) {
+                        options = [...new Set(options)];
+                    }
+                    pushAttr("vyzn.catalogue.Thickness", options);
+                if (group)
+                    pushAttr("vyzn.catalogue.Category", group);
                 if (density)
                     pushAttr("vyzn.catalogue.Density", parseFloat(density));
                 if (therm_capa)
                     pushAttr("vyzn.catalogue.ThermalCapacity", parseFloat(therm_capa));
                 if (lambda)
                     pushAttr("vyzn.catalog.ThermalConductivity", parseFloat(lambda));
+
+                if (owner)
+                    pushAttr("vyzn.catalogue.Owner", owner);
+                if (produ)
+                    pushAttr("vyzn.catalogue.Producer", produ);
+                if (license)
+                    pushAttr("vyzn.catalogue.License", license);
+                if (sourceEPE)
+                    pushAttr("vyzn.catalogue.SourceEPE", sourceEPE);
+                if (fireclass)
+                    pushAttr("vyzn.catalogue.FireClass", fireclass);
+                if (firecomb)
+                    pushAttr("vyzn.catalogue.FireComb", firecomb);
+                if (fireresis)
+                    pushAttr("vyzn.catalogue.FireResis", fireresis);
+                if (firesmoke)
+                    pushAttr("vyzn.catalogue.FireSmoke", firesmoke);
+                if (firereact)
+                    pushAttr("vyzn.catalogue.FireReact", firereact);
+                    
+
                 const updatedProduct = await request.put(new URL('/products/' + id, url).href)
                     .send({
                     "name": productName,
                     "productKey": productName,
                     "category": producerCategoryId,
                     "type": productType,
-                    "status": "approved",
+                    "status": "draft",
                     "description": null,
                     "hatchingPattern": null,
                     "attributes": attributes
@@ -227,19 +332,8 @@ export async function importMaterialsDb(url, auth, category, verbose) {
                     .set('Accept-Encoding', 'gzip, deflate, br')
                     .set('Accept-Language', 'en-US,en;q=0.5')
                     .set('Content-Type', 'application/json');
-                const materialListLink = await request.post(new URL(`/reference-material-links`, url).href)
-                    .send({
-                    "materialId": id,
-                    "referenceMaterialId": lcaProductId,
-                    "attributeGroupId": lcaAttributeGroupId
-                })
-                    .set('Authorization', authToken)
-                    .set('Content-Type', 'application/json')
-                    .set('Accept', 'application/, json')
-                    .set('Accept-Encoding', 'gzip, deflate, br')
-                    .set('Accept-Language', 'en-US,en;q=0.5')
-                    .set('Content-Type', 'application/json');
-                idx++;
+
+
                 if (singleLayer)
                     break;
             }
